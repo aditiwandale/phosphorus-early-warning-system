717906b40ff479078e1bbd4ac4776ddece993ec5
aditiwandale <145673212+aditiwandale@users.noreply.github.com>
2026-01-26 16:05:20 +0530

    Delete model.py

--- model.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-# Load old model (your local environment)
-model = tf.keras.models.load_model("lstm_risk_model.h5", compile=False)
-
-# Save in new Keras format (recommended)
-model.save("lstm_risk_model.keras")
